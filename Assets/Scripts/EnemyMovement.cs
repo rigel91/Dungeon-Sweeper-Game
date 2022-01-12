42865839242082f1e27ced2063a4304a724033a6
Rigel Kuper <rkuper@uci.edu>
2022-01-11 20:11:10 -0800

    Added Player moving with jump and camera move

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -17,7 +17,7 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             agent.SetDestination(target.position);
         }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private int playerSpeed;
+    private CharacterController controller;
 
-    private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+
+    Vector3 velocity;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundDistance = 0.4f;
+    [SerializeField]
+    private LayerMask groundMask;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        isGrounded = true;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        //checks for grounded, and makes sure that velocity.y is always -2 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");        
-        rb.velocity = new Vector3(x * playerSpeed, rb.velocity.y, z * playerSpeed);
+        float z = Input.GetAxis("Vertical");
+
+        //move player
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (move.magnitude > 1)
+        {//get rid of moveing faster when moving diagonally
+            move = move/move.magnitude;
+        }
+        controller.Move(move * speed * Time.deltaTime);
+
+        //move player downward by gravity
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
